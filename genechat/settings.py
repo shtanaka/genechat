8df7cd749c580a5891016b634e3bbb457dcce7ca
Gustavo Santiago <egustavo4@gmail.com>
2017-08-09 13:06:45 -0300

    set debug false

--- genechat/settings.py
+++ genechat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ffux*7t&obla0s25($zsunjt$p2t#xsz$xbnz_a2qm(jugj*7_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
